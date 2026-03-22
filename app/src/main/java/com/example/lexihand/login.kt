9ff6bf2c05c75c7786333e3ed5ebe0fc6bd9b1a0
MaelEsc <marcoescan66@gmail.com>
2026-03-22 01:58:46 -0600

    añadir las ventanas de la aplicacion y el registro del menu

---
+++ app/src/main/java/com/example/lexihand/login.kt
@@ -0,0 +1,47 @@
+package com.example.lexihand
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button // Importación necesaria para el botón
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Activamos el modo de pantalla completa
+        enableEdgeToEdge()
+
+        // Inflamos el diseño XML
+        setContentView(R.layout.activity_login)
+
+        // Configuramos los márgenes para las barras del sistema (Status bar)
+        // Buscamos el contenedor principal por su ID "main"
+        val mainLayout = findViewById<androidx.constraintlayout.widget.ConstraintLayout>(R.id.main)
+        if (mainLayout != null) {
+            ViewCompat.setOnApplyWindowInsetsListener(mainLayout) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+        }
+
+        // --- FORMA TRADICIONAL ---
+        // 1. Declaramos y buscamos el botón manualmente por su ID
+        val btnIngresar = findViewById<Button>(R.id.btnIngresar)
+
+        // 2. Programamos el clic
+        btnIngresar.setOnClickListener {
+            // Creamos el "Intent" para ir de esta pantalla a la MainActivity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            // Cerramos el Login para que no se quede en el historial
+            finish()
+        }
+    }
+}
